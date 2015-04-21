55acdcd5e136bd5809f919f78cfaa3caf1d52a00
Luke Lancaster <luketlancaster@gmail.com>
2015-04-21 11:09:42 -0500

    basic test setup

---
+++ test/test_cheers_integration.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+
+class TestCheersIntegration < MiniTest::Unit::TestCase
+
+  def test_that_tests_run
+    assert_equal true, false
+  end
+
+  def test_that_asserts_true
+    assert_equal true, true
+  end
+end
