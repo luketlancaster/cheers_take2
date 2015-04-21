982f1132700a09c477be269b50f0c846ab4751ea
Luke Lancaster <luketlancaster@gmail.com>
2015-04-21 14:44:32 -0500

    10 tests written

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -12,7 +12,70 @@ EOS
     assert_equal expected, output
   end
 
+  def test_one_valid_name
+    skip
+    output = `./cheers Abby`
+    expected = <<EOS
+Give me an... A
+Give me a... B
+Give me a... B
+Give me a... Y
+Abby’s just GRAND!
+EOS
+    assert_equal expected, output
+  end
+
+  def test_one_valid_name_with_hyphen
+    skip
+    output = `./cheers Abby-Sue`
+    expected = <<EOS
+Give me an... A
+Give me a... B
+Give me a... B
+Give me a... Y
+Give me a... S
+Give me an... U
+Give me an... E
+Abby-Sue’s just GRAND!
+EOS
+    assert_equal expected, output
+  end
+
+  def test_one_valid_birthday
+    skip
+    output = `./cheers 03/15`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers.rb [Name] [MM/DD Birthday]`
+EOS
+
+    assert_equal expected, output
+  end
+
+  def test_one_arg_all_non_word_characters
+    skip
+    output = `./cheers $@^@`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers.rb [Name] [MM/DD Birthday]`
+EOS
+
+    assert_equal expected, output
+  end
+
+  def test_one_arg_empty_string
+    skip
+    output = `./cheers ""`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers.rb [Name] [MM/DD Birthday]`
+EOS
+
+    assert_equal expected, output
+  end
+
   def test_two_valid_arguments
+    skip
     output = `./cheers Abby 08/25`
     expected = <<EOS
 Give me an... A
@@ -26,15 +89,41 @@ EOS
     assert_equal expected, output
   end
 
+  def test_two_invalid_arguments
+    skip
+    output = `./cheers 2344 afsae`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers.rb [Name] [MM/DD Birthday]`
+EOS
+
+    assert_equal expected, output
+  end
+
+  def test_two_args_valid_name_invalid_birthday
+    skip
+    output = `./cheers Luke we/23`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+I couldn't understand that. Could you give that to me in mm/dd format next time?
+EOS
+
+    assert_equal expected, output
+  end
+
+  def test_two_args_invalid_name_valid_birthday
+    skip
+    output = `./cheers #25 12/23`
+    expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers.rb [Name] [MM/DD Birthday]`
+EOS
+
+    assert_equal expected, output
+  end
+
   # Scenarios discussed in class:
-  # 0 args -> help message
-  # 1 valid arg
-  # 1 invalid arg
   # 2 args
-  #   * valid + valid
-  #   * valid + invalid
-  #   * invalid + valid
-  #   * invalid + invalid
   #
   # Valid Names:
   #    * Regular name
